ba5ba019346f5b311a3c594d545738279ab2867c
Marcio Michelluzzi <marciomichelluzzi@gmail.com>
2023-08-30 18:43:36 -0300

    Implementação de métodos do Controller e do Service
    Implementação de métodos do Controller e do Service

--- src/main/java/br/com/devxlabs/ravin/controllers/ProductController.java
+++ src/main/java/br/com/devxlabs/ravin/controllers/ProductController.java
@@ -3,23 +3,47 @@ package br.com.devxlabs.ravin.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.devxlabs.ravin.models.dtos.PersonDTO;
 import br.com.devxlabs.ravin.models.dtos.ProductDTO;
 import br.com.devxlabs.ravin.services.ProductService;
 
 @RestController
-@RequestMapping(value = "/api/producs")
+@RequestMapping(value = "/api/products")
 public class ProductController {
-	
+
 	@Autowired
 	ProductService service;
-	
-	@GetMapping()
-	public List<ProductDTO> listAll(){
+
+	@GetMapping
+	public List<ProductDTO> listAll() {
 		return service.listAll();
+	}
+
+	@GetMapping(value = "/{id}")
+	public ProductDTO findById(@PathVariable Long id) {
+		return service.findById(id);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public void deleteById(@PathVariable Long id) {
+		service.deleteById(id);
+	}
+	
+	@GetMapping(value = "/search")
+	public List<ProductDTO> search(
+			@RequestParam("name") String name, 
+			@RequestParam("productType") String productType,
+			@RequestParam("minSalePrice") double minSalePrice,
+			@RequestParam("maxSalePrice") double maxSalePrice
+			){
+		return service.search(name, productType, minSalePrice, maxSalePrice);
 	}
 
 }
--- src/main/java/br/com/devxlabs/ravin/services/ProductService.java
+++ src/main/java/br/com/devxlabs/ravin/services/ProductService.java
@@ -8,9 +8,23 @@ import br.com.devxlabs.ravin.models.dtos.ProductDTO;
 
 @Service
 public class ProductService {
-	
-	public List<ProductDTO> listAll(){
-		return List.of(new ProductDTO());
+
+	public List<ProductDTO> listAll() {
+		return List.of(new ProductDTO(), new ProductDTO());
+	}
+
+	public ProductDTO findById(Long id) {
+		System.out.println(id);
+		return new ProductDTO();
+	}
+
+	public void deleteById(Long id) {
+		System.out.println("Deletou o produto com id = " + id);
+	}
+
+	public List<ProductDTO> search(String name, String productType, double minSalePrice, double maxSalePrice) {
+		System.out.println("Buscando o produto com Filtros");
+		return List.of(new ProductDTO(), new ProductDTO(), new ProductDTO());
 	}
 
 }
